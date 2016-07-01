c723bef6f15868a53d9aa818c9672cebb3f2cc7f
Valdinei Reis <valdineimtz@gmail.com>
2016-07-01 16:20:09 -0300

    Autoload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+global $currentModule;
+
+spl_autoload_register(function ($class) {
+	global $currentModule;
+	if (strpos($class, 'Controller') > -1) {
+		if (file_exists('modules/'.$currentModule.'/controllers/'.$class.'.php')) {
+			require_once('modules/'.$currentModule.'/controllers/'.$class.'.php');
+		}
+	} else if (file_exists('modules/'.$currentModule.'/models/'.$class.'.php')) {
+		require_once('modules/'.$currentModule.'/models/'.$class.'.php');
+	} else {
+		require_once('core/'.$class.'.php');
+	}
+});
+
