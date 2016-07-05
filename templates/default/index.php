1ae33c3e515642d74fc966531329317ba90a41bc
Valdinei Reis <valdineimtz@gmail.com>
2016-07-04 23:38:19 -0300

    ajustando o arquivo de estilo

--- templates/default/index.php
+++ templates/default/index.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Titulo</title>
+		<link type="text/css" rel="stylesheet" href="templates/default/assets/css/style.css" media="all" />
 	</head>
 	<body>
 		<?php $this->loadViewInTemplate($viewName, $viewData); ?>
