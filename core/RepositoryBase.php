4d3f42f9a34fbb94bae1a9255afff971ebab12bb
Valdinei Reis <valdineimtz@gmail.com>
2016-07-05 11:00:38 -0300

    ativando o PDO error mode

--- core/RepositoryBase.php
+++ core/RepositoryBase.php
@@ -7,5 +7,8 @@ abstract class RepositoryBase
 	public function __construct() {
 		global $config;
 		$this->db = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['dbuser'], $config['dbpass'], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+		
+		// set the PDO error mode to exception
+    	$this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
 }
