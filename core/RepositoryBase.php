a986e26c6bc6f3006b280e1319ab0d7beec5e9b2
Valdinei Reis <valdineimtz@gmail.com>
2016-07-05 10:32:50 -0300

    add encoding padrão

--- core/RepositoryBase.php
+++ core/RepositoryBase.php
@@ -6,6 +6,6 @@ abstract class RepositoryBase
 
 	public function __construct() {
 		global $config;
-		$this->db = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['dbuser'], $config['dbpass']);
+		$this->db = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['dbuser'], $config['dbpass'], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 	}
 }
