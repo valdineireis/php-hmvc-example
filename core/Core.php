99ceffc41b9e379a493b076ee6311a13183a00d2
Valdinei Reis <valdineimtz@gmail.com>
2016-07-06 17:29:04 -0300

    extraindo os métodos

--- core/Core.php
+++ core/Core.php
@@ -2,57 +2,86 @@
 
 class Core
 {
+	private $currentController;
+	private $currentAction;
+	private $params;
+
+	public function __construct() {
+		$this->currentController = 'HomeController';
+		$this->currentAction = 'index';
+		$this->params = array();
+	}
+
 	public function run() {
-		global $currentModule;
-
-		$url = explode('index.php', $_SERVER['PHP_SELF']);
-		$url = end($url);
-		$params = array();
-
-		if (!empty($url)) {
-			
-			$url = explode('/', $url);
-			array_shift($url);
-
-			$currentModule = $url[0];
-
-			if (isset($url[1]) && strlen($url[1]) > 1) {
-				$currentController = ucfirst($url[1]).'Controller';
-			} else {
-				$currentController = 'HomeController';
-			}
-
-			if (isset($url[2]) && !empty($url[2])) {
-				$currentAction = $url[2];
-			} else {
-				$currentAction = 'index';
-			}
-
-			if (count($url) > 3) {
-				$params = $url;
-				array_splice($params, 0, 3);
-			}
-
-		} else {
-			$currentModule = 'site';
-			$currentController = 'HomeController';
-			$currentAction = 'index';
-		}
+		$this->extractUrl();
 
 		require_once 'core/Controller.php';
 
-		if (!class_exists($currentController)) {
-			$currentController = 'ErroController';
-			require_once 'core/ErroController.php';
-			$currentModule = '_erros';
-		}
+		$this->verifyController();
+		$this->verifyMethod();
 
-		$c = new $currentController();
+		$c = new $this->currentController();
+		call_user_func_array(array($c, $this->currentAction), $this->params);
+	}
 
-		if (!method_exists($c, $currentAction)) {
-			header("Location: /".$currentModule."/".lcfirst(str_replace("Controller", "", $currentController)));
+	private function extractUrl() {
+		$url = explode('index.php', $_SERVER['PHP_SELF']);
+		$url = end($url);
+
+		if (!empty($url)) {
+			$url = explode('/', $url);
+			array_shift($url);
+
+			$this->setCurrentModule($url[0]);
+
+			if (isset($url[1]) && strlen($url[1]) > 1) {
+				$this->currentController = ucfirst($url[1]).'Controller';
+			} else {
+				$this->currentController = 'HomeController';
+			}
+
+			if (isset($url[2]) && !empty($url[2])) {
+				$this->currentAction = $url[2];
+			} else {
+				$this->currentAction = 'index';
+			}
+
+			if (count($url) > 3) {
+				$this->params = $url;
+				array_splice($this->params, 0, 3);
+			}
 		} else {
-			call_user_func_array(array($c, $currentAction), $params);
+			$this->setCurrentModule('site');
 		}
+	}
+
+	private function verifyController() {
+		if (!class_exists($this->currentController)) {
+			$this->currentController = 'ErroController';
+			require_once 'core/ErroController.php';
+			$this->setCurrentModule('_erros');
+		}
+	}
+
+	private function verifyMethod() {
+		if (!method_exists($this->currentController, $this->currentAction)) {
+			if ($this->getCurrentModule() == '_erros') {
+				header("Location: /");
+			} else {
+				$controle = str_replace("Controller", "", $this->currentController);
+				header("Location: /".$this->getCurrentModule()."/".lcfirst($controle));
+			}
+			exit;
+		}
+	}
+
+	private function getCurrentModule() {
+		global $currentModule;
+		return $currentModule;
+	}
+
+	private function setCurrentModule($module = '') {
+		global $currentModule;
+		$currentModule = $module;
 	}
 }
